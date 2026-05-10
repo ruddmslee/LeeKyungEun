ce35d3134b5d9a6a765a1de388d89c54836a6d65
ruddmslee <dlruddms326@naver.com>
2026-05-11 02:43:09 +0900

    Feat: 1주차 세미나

--- django/week01/posts/admin.py
+++ django/week01/posts/admin.py
@@ -12,8 +12,8 @@ class CommentInline(admin.TabularInline):
 @admin.register(Post)
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'content', 'created_at', 'view_count']
-    search_fields = ['id']
-    search_help_text = '게시글 번호 검색이 가능합니다.'
+    search_fields = ['id', 'content']
+    search_help_text = '게시글 번호나 내용으로 검색이 가능합니다.'
     list_editable = ['content', 'view_count']
     list_filter = ['created_at']
     inlines = [CommentInline]
--- django/week01/posts/models.py
+++ django/week01/posts/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Post(models.Model):
     image = models.ImageField(verbose_name='이미지')
     content = models.TextField(verbose_name='내용')
-    created_at = models.DateTimeField(verbose_name='작성일')
+    created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
     view_count = models.PositiveIntegerField(verbose_name='조회수', default=0)
 
 class Comment(models.Model):
